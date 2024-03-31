78cf3cd466eaa1e20464047579b198f80b03e88b
kkvaldys <vladbigun677@gmail.com>
2024-03-31 05:25:19 +0300

    lesson 1 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,12 @@
-import PaymentCard from "./PaymentCard";
-
-const data1 = {
-  id: "1",
-  cardNumber: "1234 5678 9012 3456",
-  cardType: "Visa",
-  cardOwner: "John Doe",
-  date: "2024-01-01",
-  amount: 100,
-  description: "Payment for groceries",
-  isPaid: true,
-};
-
-const data2 = {
-  id: "2",
-  cardNumber: "1212 5634 9742 1111",
-  cardType: "Mastercard",
-  cardOwner: "Alice Smith",
-  date: "2024-01-05",
-  amount: 1000,
-  description: "Payment for utilities",
-  isPaid: false,
-};
+import PageTitle from "./PageTitle";
+import initialPayments from "../payments.json";
+import PaymentList from "./PaymentList";
 
 export default function App() {
   return (
     <>
-      <h1>Payments</h1>
-      <PaymentCard payment={data1} />
-      <PaymentCard payment={data2} />
+      <PageTitle text="Payments" />
+      <PaymentList payments={initialPayments} />
     </>
   );
 }
--- src/components/PaymentCard.jsx
+++ src/components/PaymentCard.jsx
@@ -1,10 +1,19 @@
 export default function PaymentCard({
-  payment: { amount, description, cardNumber, cardType, cardOwner, date },
+  payment: {
+    amount,
+    description,
+    cardNumber,
+    cardType,
+    cardOwner,
+    date,
+    isPaid,
+  },
 }) {
   return (
     <div>
       <p>Amount: {amount}</p>
-      <p>Status: Paid or Pending</p>
+      {/* {isPaid ? <p>Status: Paid</p> : <p>Status: Pending</p>} */}
+      <p>Status: {isPaid ? "paid" : "pending"}</p>
       <p>Description: {description}</p>
       <p>Card Number: {cardNumber}</p>
       <p>Card Type: {cardType}</p>
